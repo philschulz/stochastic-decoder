de653bfb22dcd4c6f5112eeb88c641b1edc6c29f
Philip Schulz <phschulz@amazon.com>
2018-05-17 12:33:46 +0000

    Fix workflow and installation script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -21,4 +21,14 @@ tar -xvzf ducttape-0.3.tgz
 export PATH=$PWD/ducttape-0.3:$PATH
 
 # multeval dependencies -> select your installer here!
-sudo $installer install autoconf automake libtool libprotobuf-c++ protobuf-compiler libprotobuf-dev ant
+sudo $installer install ant onf automake libtool pkg-config libprotobuf9v5 protobuf-compiler libprotobuf-dev
+
+if [[ $installer=="yum" ]]; then
+  echo "installing protobuf";
+  wget http://cbs.centos.org/kojifiles/packages/protobuf/2.5.0/10.el7.centos/x86_64/protobuf-2.5.0-10.el7.centos.x86_64.rpm
+  wget http://cbs.centos.org/kojifiles/packages/protobuf/2.5.0/10.el7.centos/x86_64/protobuf-devel-2.5.0-10.el7.centos.x86_64.rpm
+  wget http://cbs.centos.org/kojifiles/packages/protobuf/2.5.0/10.el7.centos/x86_64/protobuf-compiler-2.5.0-10.el7.centos.x86_64.rpm
+  sudo yum -y install protobuf-2.5.0-10.el7.centos.x86_64.rpm \
+      protobuf-compiler-2.5.0-10.el7.centos.x86_64.rpm \
+      protobuf-devel-2.5.0-10.el7.centos.x86_64.rpm 
+fi  
